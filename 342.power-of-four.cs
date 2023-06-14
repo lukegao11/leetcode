09a9730ee1a383f7dfd5dc96d782030f0005d829
lukegao11 <guanqun.gao@marylandsmith.umd.edu>
2023-06-14 16:47:47 -0400

    Update 342.power-of-four.cs

--- 342.power-of-four.cs
+++ 342.power-of-four.cs
@@ -7,11 +7,15 @@
 // @lc code=start
 public class Solution
 {
-    public bool IsPowerOfFour(int n)
+    public bool IsPowerOfFour(int num)
     {
-        if (n == 0) return false;
-        while (n % 4 == 0) n /= 4;
-        return n == 1;
+        
+        // if (num == 0) return false;
+        // while (num % 4 == 0) num /= 4;
+        // return num == 1;
+
+        //bitwise
+        return (num > 0) && ((num & (num - 1)) == 0) && ((num & 0xaaaaaaaa) == 0);
     }
 }
 // @lc code=end
